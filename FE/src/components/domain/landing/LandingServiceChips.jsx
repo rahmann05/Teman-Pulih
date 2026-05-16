2b74022f2121c37b89fa9a1b93c48b943e2dbf8d
rahman <safdar23@if.unjani.ac.id>
2026-05-16 13:55:30 +0700

    fix: margin dan padding landing page desktop dan tablet

--- FE/src/components/domain/landing/LandingServiceChips.jsx
+++ FE/src/components/domain/landing/LandingServiceChips.jsx
@@ -34,8 +34,8 @@ const LandingServiceChips = () => {
       <svg style={{ visibility: 'hidden', position: 'absolute' }} width="0" height="0">
         <defs>
           <filter id="goo" x="-50%" y="-50%" width="200%" height="200%">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 40 -15" result="goo" />
+            <feGaussianBlur in="SourceGraphic" stdDeviation="7" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -12" result="goo" />
             <feComposite in="SourceGraphic" in2="goo" operator="atop" />
           </filter>
         </defs>
